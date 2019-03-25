1563c77837c77a2f94b99e6dba1e86422abfb58a
sahil_21 <ssashi2@illinois.edu>
2019-03-25 15:55:36 -0500

    Model tto calculate and store probabilities created

---
+++ NaiveBayes 2/src/PixelData.cpp
@@ -0,0 +1,9 @@
+//
+//  PixelData.cpp
+//  NaiveBayes 2
+//
+//  Created by sahil sashi on 3/25/19.
+//  Copyright © 2019 sahil sashi. All rights reserved.
+//
+
+#include "PixelData.hpp"
---
+++ NaiveBayes 2/src/PixelData.hpp
@@ -0,0 +1,14 @@
+//
+//  PixelData.hpp
+//  NaiveBayes 2
+//
+//  Created by sahil sashi on 3/25/19.
+//  Copyright © 2019 sahil sashi. All rights reserved.
+//
+
+#ifndef PixelData_hpp
+#define PixelData_hpp
+
+#include <stdio.h>
+
+#endif /* PixelData_hpp */
