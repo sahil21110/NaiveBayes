73f93d8bed52cd090c812cd14ed93f5c4bbbeb02
sahil_21 <ssashi2@illinois.edu>
2019-03-27 00:02:17 -0500

    Data storage working, algorithmcomplete

---
+++ NaiveBayes 2/src/catch.hpp
@@ -0,0 +1,13 @@
+//
+//  catch.hpp
+//  NaiveBayes 2
+//
+//  Created by sahil sashi on 3/26/19.
+//  Copyright © 2019 sahil sashi. All rights reserved.
+//
+
+#ifndef catch_h
+#define catch_h
+
+
+#endif /* catch_h */
